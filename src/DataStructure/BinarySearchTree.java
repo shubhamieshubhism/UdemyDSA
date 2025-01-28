c45197e16544144f7391f0c6daff7e4b1d57e37c
shubhamieshubhism <shubhamtirpude20@gmail.com>
2025-01-28 18:32:10 +0530

    BST Insert Method

--- src/DataStructure/BinarySearchTree.java
+++ src/DataStructure/BinarySearchTree.java
@@ -1,14 +1,42 @@
 package DataStructure;
 
 public class BinarySearchTree {
-     public Node root;
-}
+    public Node root;
 
-class Node{
-    int value;
-    Node left;
-    Node right;
-    Node(int value){
-        this.value=value;
+
+    public class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public boolean insert(int value){
+        Node newNode = new Node(value);
+        if(root==null){
+            root=newNode;
+            return true;
+        }
+        Node temp = root;
+        while(true){
+            //to identify the duplicates in the tree
+            if(newNode.value==temp.value) return false;
+            if(newNode.value<temp.value){
+                if(temp.left==null){
+                    temp.left=newNode;
+                    return true;
+                }
+                temp=temp.left;
+            }else{
+                if(temp.right==null){
+                    temp.right=newNode;
+                    return true;
+                }
+                temp=temp.right;
+            }
+        }
     }
 }
