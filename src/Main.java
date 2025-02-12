dd4d07240cbe2a307eabf45c3d261b8f70425c8b
shubhamieshubhism <shubhamtirpude20@gmail.com>
2025-02-12 19:02:07 +0530

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-import DataStructure.DoublyLinkedList;
-import DataStructure.LinkedList;
-import DataStructure.Queue;
-import DataStructure.Stack;
+import DataStructure.*;
 
 public class Main {
     public static void main(String[] args) {
-        Queue myQueue = new Queue(7);
-        myQueue.enqueue(9);
-        myQueue.printQueue();
-        myQueue.getLength();
+        BinarySearchTree myBST = new BinarySearchTree();
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
 
+        myBST.insert(27);
+
+        System.out.println(myBST.root.right.value);
 
 
 
